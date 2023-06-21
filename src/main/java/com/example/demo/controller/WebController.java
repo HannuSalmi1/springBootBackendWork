ae75faa2157089e5121b905dfa9beb29934b3066
HannuSalmi1 <hannusalmi1@outlook.com>
2023-06-22 01:34:28 +0300

    post request return response to user
    post request return response to user

--- src/main/java/com/example/demo/controller/WebController.java
+++ src/main/java/com/example/demo/controller/WebController.java
@@ -8,6 +8,7 @@ import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -33,14 +34,14 @@ public class WebController {
 
     //user sign in
     @PostMapping(value = "/SignIn", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public User saveUser( @RequestBody User user, UrlCreator urlCreator) throws IOException {
+    public ResponseEntity<String> saveUser(@RequestBody User user, UrlCreator urlCreator) throws IOException {
 
         String name = user.getName();
         urlCreator.creatingFolder(user);
+        userService.saveUser(user);
 
 
-
-        return userService.saveUser(user);
+        return ResponseEntity.ok("new user added to database");
     }
 
 }
